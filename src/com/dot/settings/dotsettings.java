b28d4f02c561ad8404a54d54555e557f8d7a9dee
ganeshi4u <ganeshvarmai4u@gmail.com>
2017-08-13 13:01:26 +0530

    added settings preference fragment and metrics
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- src/com/dot/settings/dotsettings.java
+++ src/com/dot/settings/dotsettings.java
@@ -4,9 +4,11 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.SettingsPreferenceFragment;
 import android.os.Bundle;
 
-public class dotsettings extends AppCompatActivity {
+public class dotsettings extends SettingsPreferenceFragment {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +17,10 @@ public class dotsettings extends AppCompatActivity {
         getFragmentManager().beginTransaction()
                 .replace(R.id.pref_card0, new PrefsFragment())
                 .commitAllowingStateLoss();
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.APPLICATION;
     }
 }
